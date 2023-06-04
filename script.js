a280d1a1f12e36bfc05957505527f12fa6d0e3be
debajitsaha <81859187+debajitsaha89@users.noreply.github.com>
2023-06-04 11:47:10 +0530

    done

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 let head=document.getElementsByTagName('h1')
 head[1].addEventListener('click',()=>{head[1].innerHTML="Hello World"})
+//Q,5
+let repl=document.getElementById('replace');
+function replace(){
+ repl.innerHTML="Welcome to Elevation academy";
+}
+//Q.6
+let count=0;
+head[2].addEventListener('click',()=>{
+  
+    if(count%2===0)
+    {
+        head[2].style.color="red";
+        count++;
+    }
+    else{
+        head[2].style.color="blue";
+        head[2].innerHTML="Hello World"
+        count--; 
+    }
+})
+//Q.7
 
